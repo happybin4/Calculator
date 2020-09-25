f39f12da3c885600f55452b8913e1b80b4ba5b9d
성빈 안 <happybin4@naver.com>
2020-09-25 18:20:54 +0900

    초기 커밋.

---
+++ Calculator/Calc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calc
+    {
+        int fnum;
+        int snum;
+        public int Fnum { get; set; }
+        public bool sum(int result)
+        {
+            return true;
+        }
+        public int sub(int fnum,int snum)
+        {
+            return fnum - snum;
+        }
+        public int mul(int fnum,int snum)
+        {
+            return fnum * snum;
+        }
+        public double div(int fnum,int snum)
+        {
+            return fnum / snum;
+        }
+       
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //계산기 ㄱㄱ
+            //난수 구하기
+            int fnum; //퍼스트넘버
+            int snum; //세컨드넘버
+            string op;
+            string[] arrOp = new string[4] { "+", "-", "*", "/" }; //연산자 배열
+            Random rand = new Random();
+            Calc c1 = new Calc();
+            while (true)
+            {   
+                fnum = rand.Next(10, 99);
+                snum = rand.Next(10, 99);
+                op = arrOp[rand.Next(0, 3)]; // 연산자 난수로다가 정함!
+                Console.WriteLine("========랜덤문제!========");
+                Console.WriteLine($"{fnum} {op} {snum} = ");
+                if(op == "/")                
+                    Console.WriteLine("나누기는 몫만 구하세요!");
+                int result;
+                if(!int.TryParse(Console.ReadLine(),out result))
+                {
+                    Console.WriteLine("다시 입력하세요");
+                }
+
+                switch (op)
+                {
+                    case "+": c1.sum(fnum, snum); break;
+                    case "-": break;
+                    case "*": break;
+                    case "/": break;
+                }
+            }
+            
+            
+
+
+        }
+    }
+}
